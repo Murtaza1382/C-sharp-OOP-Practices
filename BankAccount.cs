283727a819cad7c36391917d37bab56b05d13f59
Murtaza Rahimi <murtaza2003rahimi@gmail.com>
2026-08-31 18:19:21 +0430

    The class codes generated

---
+++ BankAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    internal class BankAccount
+    {
+// Fields / Insatnce Variables 
+        private string accountNumber;
+        private string ownerName;
+        private decimal balance;
+
+// Constructor
+        public BankAccount(string accountNumber, string ownerName, decimal initialBalance)
+        {
+            this.accountNumber = accountNumber;
+            this.ownerName = ownerName;
+            balance = initialBalance;
+        }
+
+        // Mehtods
+        public void Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"Deposited: {amount}");
+            }
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                balance -= amount;
+                Console.WriteLine($"Withdrawn: {amount}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid withdrawal.");
+            }
+        }
+
+        public void DisplayAccountInfo()
+        {
+            Console.WriteLine("----- BANK ACCOUNT -----");
+            Console.WriteLine($"Account Number: {accountNumber}");
+            Console.WriteLine($"Owner: {ownerName}");
+            Console.WriteLine($"Balance: {balance}");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Classes;
+BankAccount account1 =
+    new BankAccount("ACC-001", "Murtaza", 1000);
+
+account1.Deposit(500);
+
+account1.Withdraw(200);
+
+account1.DisplayAccountInfo();
