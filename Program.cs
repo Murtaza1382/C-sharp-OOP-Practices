9e8a2f516c3d0431572fcc88184213d41d8a6a88
Murtaza Rahimi <murtaza2003rahimi@gmail.com>
2026-08-31 20:53:55 +0430

    The Anonymous Object is added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using Classes;
+
+// class
 BankAccount account1 =
     new BankAccount("ACC-001", "Murtaza", 1000);
 
@@ -7,3 +9,6 @@ account1.Deposit(500);
 account1.Withdraw(200);
 
 account1.DisplayAccountInfo();
+
+// anonymous object
+new BankAccount("ACC-001", "Murtaza", 1000).DisplayAccountInfo();
